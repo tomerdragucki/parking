94451638f0c85bc8dede0ab5fae1269259a2a550
tomerdragucki <tomerd@campus.technion.ac.il>
2021-10-05 22:57:46 +0300

    add files

---
+++ parking.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+
+def get_availability(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    img = soup.find(id="ctl06_data1_lblAhuzotIDStat_0").find_all('img')[0]['src']
+    if img ==  '/pics/ParkingIcons/meat.png':
+        return "Few places"
+    elif img == '/pics/ParkingIcons/male.png':
+        return "Parking is full"
+    elif img == '/pics/ParkingIcons/panui.png':
+        return "Parking is available"
+
+
+st.title("Sokolov gang parking info")
+
+"Assuta: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=122")
+"Basel: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=3")
+"Arlozorov 17: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=123")
+
+# print("Assuta: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=122"))
+# print("Basel: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=3"))
+# print("Arlozorov 17: " + get_availability("http://www.ahuzot.co.il/Parking/ParkingDetails/?ID=123"))
